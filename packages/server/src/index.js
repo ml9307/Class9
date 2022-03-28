65fbf691a76cefcc87ffe2a519f1e2a651ddc7af
ml9307 <ml9307@g.rit.edu>
2022-03-28 23:11:11 +0000

    Class work

---
+++ packages/server/src/index.js
@@ -0,0 +1,12 @@
+import { ApolloServer } from "apollo-server";
+import { resolvers } from "./resolvers.js";
+import { typeDefs } from "./typeDefs.js";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await server.listen();
+
+console.log(`Server ready at ${url}`);
